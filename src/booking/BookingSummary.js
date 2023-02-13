12c9fac5619497f8244b14a95fc1f371e6efbb47
Ankit Vishwakarma <ankitvish1306@gmail.com>
2023-02-13 18:38:13 +0530

    completed seat page

--- src/booking/BookingSummary.js
+++ src/booking/BookingSummary.js
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './BookingComponent.css';
+import { useLocation } from 'react-router-dom';
+
+
 
 function Booking() {
+    var location = useLocation();
+    var numberOfSeats = location.state.seats;
+    var totalPrice = location.state.price;
     return(
         <>
-        <h1>this is booking summary page</h1>
+        <h1>this is booking summary page price is {totalPrice} number of seats {numberOfSeats}</h1>
         </>
     );
 }
--- src/seats/SeatComponent.js
+++ src/seats/SeatComponent.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import './SeatComponent.css';
 import React, { useEffect, useState } from "react";
 import HostUrl from '../HostUrl.json'
+import { Link } from 'react-router-dom';
 
 function Seat() {
     var location = useLocation();
@@ -10,6 +11,8 @@ function Seat() {
     var theaterId = location.state.theaterId;
     var movieId = location.state.movieId;
     const [items, setItems] = useState([])
+    const [priceOfOneSeat, setPricePerOneSeat] = useState(0)
+    const [numberOfSeats, setNumberOfSeats] = useState(0)
 
     useEffect(() => {
         fetch(`${HostUrl.hostUrl}/Screen`)
@@ -21,21 +24,32 @@ function Seat() {
             )
     }, [])
 
+    const seatClicked = (e) => {
+        debugger
+        var price = e.target.id
+        var totalPrice = Number(price) + priceOfOneSeat
+        setPricePerOneSeat(totalPrice)
+        var seatCount = totalPrice / Number(price)
+        setNumberOfSeats(seatCount)
+    }   
+
     const getScreen =
         items.filter(screen => screen.movieId == movieId && screen.theaterId == theaterId).map(filteredTheater => (
-            [...Array(filteredTheater.totalSeats)].map((e, i) =><a href="#" key={i}>1</a>)
+            [...Array(filteredTheater.totalSeats)].map((e, i) => <a href="javascript:;" id={filteredTheater.pricePerSeat} onClick={seatClicked} key={i}>*</a>)
         ))
 
     return (
         <>
             <h1>this is seat page and the show id is {showId} theater id is {theaterId} movie id is {movieId}</h1>
             <h1>{items.length}</h1>
+            {priceOfOneSeat}<br></br>
+            {numberOfSeats}
             <div className='seat-page'>
-                <div>
                 {getScreen}
-                </div>
             </div>
-            
+            <div>
+                <Link to="/booking" state={{ seats : numberOfSeats, price : priceOfOneSeat}}>Book Ticket</Link>
+            </div>
         </>
     );
 }
